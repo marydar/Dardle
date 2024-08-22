968f805363b45fa1cb296618cae98c05bfaaffb7
Maryam Dar <maryamdar1384@gmail.com>
2024-08-23 01:50:38 +0330

    add database

--- controls.js
+++ controls.js
@@ -1,42 +1,71 @@
 let text="";
-let target = "reoct";
+let total=0;
+let win =0;
+let target = "react";
 var keyMap = new Map();
 let cells = [];
 const rows = 6;
 const column = 5;
 let rowCounter =0;
 let colCounter =0;
+let words = [];
+
+fetch("./words.json")
+    .then(response => response.json())
+    .then(value => {
+        value.forEach(obj => {
+            words.push(obj.word);
+        });
+        target = words[randomNumber];
+    });
+
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+      
+const randomNumber = getRandomNumberBetween(0, 2000);
+
 
 function closeAlert(){
     console.log("koko");
     document.getElementById("alert").classList.remove("showAlert");
     document.getElementById("alert").classList.add("removeAlert");
-    location.reload();
+    // location.reload();
+}
+function closeError(){
+    document.getElementById("error").classList.remove("error-show");
+    document.getElementById("error").classList.add("error-close");
+}
+function showError(){
+    document.getElementById("error").classList.remove("error-close");
+    document.getElementById("error").classList.add("error-show");
+    setTimeout(closeError, 2000);
+}
+function restartClick(){
+    closeAlert();
+    restart();
+}
+function restart(){
+    rowCounter=0;
+    colCounter=0;
+    const randomNumber = getRandomNumberBetween(0, 2000);
+    target = words[randomNumber];
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < column; j++) {
+            cells[i][j].removeAttribute('class');
+            cells[i][j].classList.add("grid-item");
+            cells[i][j].innerHTML = "";
+        }
+    }
+    const divs = document.getElementsByClassName("key");
+    const divArray = Array.from(divs);
+    divArray.forEach(div => {
+        div.removeAttribute('class');
+        div.classList.add("key");
+    });
 }
 
-// var data = $.csv.toObjects(csv);
-// async function fetchData() {
-//     try {
-//       const response = await fetch('s.json'); // Replace with your file path
-//       const jsonData = await response.json();
-  
-//       const wordsArray = jsonData.map(item => item.word);
-  
-//       // Do something with the wordsArray, e.g.,
-//       wordsArray.forEach(word => {
-//         console.log(word);
-//       });
-//     } catch (error) {
-//       console.error('Error fetching or parsing JSON:', error);
-//     }
-//   }
-  
-//   fetchData();
-
-// console.log(json, 'the json obj');
-
 function pressedChar(char){
-    console.log(char.toUpperCase());
     if(char.toUpperCase()=="BACKSPACE"){
         if(colCounter>0){
             text= text.slice(0, -1);
@@ -72,41 +101,69 @@ function checkForYellow(char){
     return false;
 }
 function checkCharacters(){
-    //if the word is in list
-    for(let i = 0; i< column; i++){
-        let char = text.charAt(i);
-        cells[rowCounter][i].classList.remove("scale");
 
-        if(target.toUpperCase().includes(char)){
-            if(target.charAt(i).toUpperCase()==char){
-                cells[rowCounter][i].classList.add("green-cell");
-                if(checkForGreen(char)){
-                    keyMap.get(char).classList.add("greenBack");
+    if(!words.includes(text.toLowerCase())){
+        showError();
+    }
+    else{
+        for(let i = 0; i< column; i++){
+            let char = text.charAt(i);
+            cells[rowCounter][i].classList.remove("scale");
+
+            if(target.toUpperCase().includes(char)){
+                if(target.charAt(i).toUpperCase()==char){
+                    cells[rowCounter][i].classList.add("green-cell");
+                    if(checkForGreen(char)){
+                        keyMap.get(char).classList.add("greenBack");
+                    }
+                }
+                else{
+                    cells[rowCounter][i].classList.add("yellow-cell");
+                    if(checkForYellow(char)){
+                        keyMap.get(char).classList.add("yellowBack");
+                    }
                 }
             }
             else{
-                cells[rowCounter][i].classList.add("yellow-cell");
-                if(checkForYellow(char)){
-                    keyMap.get(char).classList.add("yellowBack");
-                }
+                cells[rowCounter][i].classList.add("gray-cell");
+                keyMap.get(char).classList.add("grayBack");
             }
         }
-        else{
-            cells[rowCounter][i].classList.add("gray-cell");
-            keyMap.get(char).classList.add("grayBack");
+        rowCounter++;
+        colCounter=0;
+        if(text==target.toUpperCase()){
+            console.log("win");
+            win++;
+            total++;
+            setTimeout(showWinner, 2000);
+            // show();
         }
+        else if(rowCounter==rows){
+            console.log("lost");
+            total++;
+            setTimeout(showLoser, 2000);
+            
+        }
+        text="";
     }
-    if(text==target.toUpperCase()){
-        document.getElementById("alert").classList.remove("removeAlert");
-        document.getElementById("alert").classList.add("showAlert");
-    }
-    rowCounter++;
-    colCounter=0;
-    text="";
+
+}
+function showWinner(){
+    document.getElementById("alert").classList.remove("removeAlert");
+    document.getElementById("alert").classList.add("showAlert");
+    document.getElementById("status").innerHTML = "you've won!";
+    document.getElementById("score").innerHTML = `score : ${win}/${total}`;
+    document.getElementById("answer").innerHTML = "The answer : "+target;
+}
+function showLoser(){
+    document.getElementById("alert").classList.remove("removeAlert");
+    document.getElementById("alert").classList.add("showAlert");
+    document.getElementById("status").innerHTML = "you've lost!";
+    document.getElementById("score").innerHTML = `score : ${win}/${total}`;
+    document.getElementById("answer").innerHTML = "The answer : "+target;
 }
 
 function whichButton(event){
-    console.log(event.keyCode);
     if((event.keyCode>=65 && event.keyCode<=90 )|| event.keyCode==8 || event.keyCode==13 ){
         pressedChar(event.key);
     }
@@ -116,7 +173,9 @@ function keyClicked(element) {
 }
 const SPEED = 0.5;
 document.addEventListener('DOMContentLoaded', () => {
+
     document.getElementById("alert").classList.add("removeAlert");
+    document.getElementById("error").classList.add("error-close");
     for (let i = 0; i < rows; i++) {
         cells[i] = [];
         for (let j = 0; j < column; j++) {
